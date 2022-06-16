f8bdddb981c56a9dfd91fb2bd720c423319cd6a5
Liam Maeder <liam.maeder@gmail.com>
2022-06-16 17:13:51 +0200

    comitting build changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Navbar from "./components/Navbar";
 import Card  from "./components/Card";
 import data from "./data/data.json";
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 function Card(props) {
     return (
         <div className="card">
@@ -6,7 +8,7 @@ function Card(props) {
                 <div className="card--details">
                     <div className="card-country">                        
                         <span className="card-location-name"><img src={require("../images/location-icon-2.png")} alt="location-icon" className="location-icon"/>&nbsp;{props.country}</span>
-                        <span className="location-maps-link"><a href={`https://maps.google.com/?q=${props.location}`} target="_blank" rel="noreferrer">View on Google Maps</a></span>
+                        <span className="location-maps-link"><a href={`https://maps.google.com/?q=${props.location}`} target="_blank" rel="noopener noreferrer">View on Google Maps</a></span>
                     </div>
                     <div className="card-header">
                         <h2 className="header-title">{props.location}</h2>

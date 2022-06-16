690bf32a618849232dd3eef8a27fa64ab01f4f63
Liam Maeder <liam.maeder@gmail.com>
2022-06-16 16:33:13 +0200

    committing my final changes to the solo project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Navbar from "./components/Navbar";
 import Card  from "./components/Card";
+import data from "./data/data.json";
 
 function App() {
+    const cards = data.map(item => {
+        return (
+            <Card 
+                key={item.id} 
+                {...item}
+            />
+        )
+    })
+
     return (
         <div className="App">
             <Navbar />
-            <Card />
+            <div className="container">
+                {cards}
+            </div>
         </div>
     );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,23 @@
-function Card() {
+function Card(props) {
     return (
-        <h1>This is the card</h1>
+        <div className="card">
+            <div className="card--content">
+                <img src={require(`../images/${props.locationImg}`)} alt={"location"} className="card-image"/>
+                <div className="card--details">
+                    <div className="card-country">                        
+                        <span className="card-location-name"><img src={require("../images/location-icon-2.png")} alt="location-icon" className="location-icon"/>&nbsp;{props.country}</span>
+                        <span className="location-maps-link"><a href={`https://maps.google.com/?q=${props.location}`} target="_blank" rel="noreferrer">View on Google Maps</a></span>
+                    </div>
+                    <div className="card-header">
+                        <h2 className="header-title">{props.location}</h2>
+                        <h5 className="header-date">{props.date}</h5>
+                    </div>
+                    <div className="card-description">
+                        <p>{props.description}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
